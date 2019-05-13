663b840203ceebf8ca9356cde66749d7be87976e
Bahrudin Hrnjica <bhrnjica@bahr-sb2.com>
2019-05-13 08:32:11 +0200

    9 predavanje

---
+++ 09 Predavanje/CIDWebDemo/XUnitTestProject1/HomeControllerTest.cs
@@ -0,0 +1,64 @@
+using CIDWebDemo.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using System;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.Equal("Your application description page.", result.ViewData["Message"]);
+        }
+
+        [Fact]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+
+        [Fact]
+        public void Contact2()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
